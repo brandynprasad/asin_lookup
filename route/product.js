6b85b68188d0737b615351e302b8f153a9a218b6
Brandyn Prasad <brandyn@fintechdev.net>
2018-06-08 03:52:43 -0700

    Implement scraping of best sellers rank

--- route/product.js
+++ route/product.js
@@ -29,14 +29,27 @@ function findProductByAsin(req, res, next) {
           let category = [...document.querySelectorAll('#nav-subnav')]
             .map(node => node.getAttribute('data-category'))[0].replace('-', ' ');
 
-          // GET DIMENSIONS
           let dimensions = '';
+          let rank       = '';
+
           document.querySelectorAll('*').forEach((node) => {
+            // GET DIMENSIONS
+
+            if (node.textContent.includes('Item Dimensions') && dimensions == '') {
+              dimensions = node.textContent.substring(
+                node.textContent.lastIndexOf('L x W x H'),
+                node.textContent.lastIndexOf(' inches')
+              );
+              dimensions = `${dimensions.slice(9).replace(/\s/g, '')}`
+            }
+
             if (node.textContent.includes('Product Dimensions') && dimensions == '') {
               dimensions = node.textContent.substring(
                 node.textContent.lastIndexOf('Product Dimensions'),
                 node.textContent.lastIndexOf(' inches')
               );
+
+              dimensions = `${dimensions.slice(18).replace(/\s/g, '')}`
             }
 
             if (node.textContent.includes('Package Dimensions') && dimensions == '') {
@@ -44,13 +57,30 @@ function findProductByAsin(req, res, next) {
                 node.textContent.lastIndexOf('Package Dimensions'),
                 node.textContent.lastIndexOf(' inches')
               );
+
+              dimensions = `${dimensions.slice(18).replace(/\s/g, '')}`
+            }
+
+            // GET RANK
+            if (node.textContent.includes('Best Sellers Rank') && rank == '') {
+
+              rank = node.textContent.substring(
+                node.textContent.lastIndexOf('Best Sellers Rank'),
+                node.textContent.lastIndexOf('Warranty')
+              );
+
+              rank = rank.substring(
+                rank.lastIndexOf('Best Sellers Rank'),
+                rank.indexOf('in')
+              );
+
+              rank = rank.match(/\d|[0-9]+/g).map(Number).toString().replace(/,/g, '')
             }
           });
 
           // Final formatting of dimensions
           if (dimensions !==  '') {
             const rg = new RegExp("\\" + 'x', "g");
-            dimensions = `${dimensions.slice(18).replace(/\s/g, '')} inches`
             dimensions = dimensions.replace(rg, " x ");
 
             if (dimensions[0] === ":") {
@@ -58,7 +88,7 @@ function findProductByAsin(req, res, next) {
             }
           }
 
-          return { category, dimensions };
+          return { category, dimensions, rank };
         });
 
         await console.log(result);
