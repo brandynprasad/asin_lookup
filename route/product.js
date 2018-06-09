eae35bed5181061715c1160e7711053c250c0654
Brandyn Prasad <brandyn@fintechdev.net>
2018-06-09 08:34:59 -0700

    Add simple views and refactor scraping to avoid some bad data

--- route/product.js
+++ route/product.js
@@ -15,12 +15,12 @@ function findProductByAsin(req, res, next) {
 
     if (product) {
       req.logger.verbose(`Sending product ${product._id} to client`);
-      res.status(200).send(product);
+      res.render('product', { product });
     }
     else {
       (async () => {
         const browser = await puppeteer.launch();
-        const page = await browser.newPage();
+        const page    = await browser.newPage();
 
         await page.goto('https://www.amazon.com');
         await page.goto(`https://www.amazon.com/dp/${req.params.asin}`)
@@ -28,7 +28,7 @@ function findProductByAsin(req, res, next) {
         const result = await page.evaluate(() => {
           // GET CATEGORY
           let category = [...document.querySelectorAll('#nav-subnav')]
-            .map(node => node.getAttribute('data-category'))[0].replace('-', ' ');
+            .map(node => node.getAttribute('data-category'))[0].replace(/-/g, ' ');
 
           let dimensions = '';
           let rank       = '';
@@ -37,6 +37,7 @@ function findProductByAsin(req, res, next) {
 
             // GET DIMENSIONS
             if (node.textContent.includes('Item Dimensions') && dimensions == '') {
+
               dimensions = node.textContent.substring(
                 node.textContent.lastIndexOf('L x W x H'),
                 node.textContent.lastIndexOf(' inches')
@@ -44,13 +45,15 @@ function findProductByAsin(req, res, next) {
               dimensions = `${dimensions.slice(9).replace(/\s/g, '')}`
             }
 
-            if (node.textContent.includes('Product Dimensions') && dimensions == '') {
-              dimensions = node.textContent.substring(
-                node.textContent.lastIndexOf('Product Dimensions'),
-                node.textContent.lastIndexOf(' inches')
-              );
+            if (node.textContent.includes('Product Dimensions') && dimensions == '' ||
+              node.textContent.includes('Product Dimensions') && dimensions.length > 30) {
 
-              dimensions = `${dimensions.slice(18).replace(/\s/g, '')}`
+                dimensions = node.textContent.substring(
+                  node.textContent.lastIndexOf('Product Dimensions'),
+                  node.textContent.lastIndexOf(' inches')
+                );
+
+                dimensions = `${dimensions.slice(18).replace(/\s/g, '')}`
             }
 
             if (node.textContent.includes('Package Dimensions') && dimensions == '') {
@@ -65,17 +68,33 @@ function findProductByAsin(req, res, next) {
             // GET RANK
             if (node.textContent.includes('Best Sellers Rank') && rank == '') {
 
-              rank = node.textContent.substring(
-                node.textContent.lastIndexOf('Best Sellers Rank'),
-                node.textContent.lastIndexOf('Warranty')
-              );
+              try {
+                rank = node.textContent.substring(
+                  node.textContent.lastIndexOf('Best Sellers Rank'),
+                  node.textContent.lastIndexOf('Warranty')
+                );
 
-              rank = rank.substring(
-                rank.lastIndexOf('Best Sellers Rank'),
-                rank.indexOf('in')
-              );
+                rank = rank.substring(
+                  rank.lastIndexOf('Best Sellers Rank'),
+                  rank.indexOf('in')
+                );
 
-              rank = rank.match(/\d|[0-9]+/g).map(Number).toString().replace(/,/g, '')
+                rank = rank.match(/\d|[0-9]+/g).map(Number).toString().replace(/,/g, '');
+              } catch(err) {
+                console.log(err);
+
+                rank = node.textContent.substring(
+                  node.textContent.lastIndexOf('Best Sellers Rank'),
+                  node.textContent.lastIndexOf('Shipping Weight')
+                );
+
+                rank = rank.substring(
+                  rank.lastIndexOf('Best Sellers Rank'),
+                  rank.indexOf('in')
+                );
+
+                rank = rank.match(/\d|[0-9]+/g).map(Number).toString().replace(/,/g, '');
+              }
             }
           });
 
@@ -98,19 +117,39 @@ function findProductByAsin(req, res, next) {
           dimensions: result.dimensions,
           rank      : result.rank,
         }, (err, product) => {
-          if (err) { return next(err); }
+          if (err) {
+            req.logger.error(err);
+            return res.status(404).end();
+          }
 
           browser.close();
 
-          req.logger.verbose(`Product ${product._id} written to db. Sending product to client`);
-          res.status(201).send(product);
+          req.logger.verbose(`Product ${product._id} created. Sending product to client`);
+          res.render('product', { product });
         });
       })();
     }
   });
 }
 
+function queryProducts(req, res, next) {
+  req.logger.info('Querying products', req.query);
+  req.model('Product').countAndFind(req.query)
+    .exec((err, products, productCount) => {
+      if (err) { return next(err); }
+
+      req.logger.verbose('Sending products to client');
+      res.render('products', { products, productCount });
+    });
+}
+
+function search(req, res, next) {
+  res.render('search');
+}
+
 
 router.get('/product/:asin', findProductByAsin);
+router.get('/products', queryProducts);
+router.get('/search', search);
 
 module.exports = router;
